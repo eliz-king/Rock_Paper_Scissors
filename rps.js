d2617372511b2409ecda995e57d7a139c8e86001
David Sarkisian <dsarkisian@plansource.com>
2019-03-21 00:56:36 -0600

    finished the page. pushing up a working copy

--- rps.js
+++ rps.js
@@ -1,33 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   rps = ['Rock', 'Paper', 'Scissors'];
-
-
   //variables
+  var choicePicker = document.getElementById("choice-picker");
   var usersChoice;
-  // var computerChoice = compChoice(rps)
-
+  var result;
   // HTML elements 
-  var startButton = document.getElementById('start-button')
-  var userNameField = document.getElementById('user-name')
-  var game = document.getElementById('game')
-
-  var rock = document.getElementById("choice1")
-  var paper = document.getElementById("choice2")
-  var scissors = document.getElementById("choice3")
-
+  var startButton = document.getElementById('start-button');
+  var userNameField = document.getElementById('user-name');
+  var game = document.getElementById('game');
+  var submitButton = document.getElementById('submit-choice');
+  var tracker = [];
+  var resultEl = document.getElementById('result');
+  var userChoiceEl = document.getElementById('usersChoice');
+  var compChoiceEl = document.getElementById('compsChoice');
+  var trackerEl = document.getElementById('tracker')
   // back end functions
 
   function compChoice(rps) {
     var choice = rps[Math.floor(Math.random() * rps.length)];
-    console.log(choice)
     return choice;
   }
 
   function winLose(computerChoice, usersChoice, rps) {
     var result = null
     // debugger
-    if (usersChoice == compChoice) {
+    if (usersChoice == computerChoice) {
       result = 'Draw';
     }
     else if (((usersChoice == rps[0]) && (computerChoice == rps[2])) || ((usersChoice == rps[1]) && (computerChoice == rps[0])) || ((usersChoice == rps[2]) && (computerChoice == rps[1]))) {
@@ -36,23 +34,56 @@ document.addEventListener("DOMContentLoaded", function () {
     else {
       result = 'Computer Won';
     }
-    console.log(result);
+    
+    tracker.push(result);
+    console.log(result, tracker);
+    printResults(result);
     return result;
   }
 
+  function renderGame() {
+    game.className = "game-field-show";
+  }
 
-  function registerChoice(usersChoice) {
-    // var usersChoice = null
-    rock.addEventListener('click', function () {
-      return usersChoice = rps[0]
-    })
-    paper.addEventListener('click', function () {
-      return usersChoice = rps[1]
-    })
-    scissors.addEventListener('click', function () {
-      return usersChoice = rps[2]
-    })
-  };
+  function printTracker (tracker) {
+    // var resultString = tracker[0];
+    // for (var i = 1; i<tracker.length; i++) {
+    //   resultString += result + ","
+    // }
+
+    trackerEl.innerHTML = "Current results:  " + tracker.join(', ')
+  }
+
+  function printCompsChoice() {
+    cChoice = compChoice(rps)
+    console.log("Computers choice: " + cChoice)
+
+    compChoiceEl.innerHTML = "Computers choice: " + cChoice;
+  }
+
+
+  function makeChoice() {
+    usersChoice = choicePicker.options[choicePicker.selectedIndex].text
+    printUsersChoice()
+    return usersChoice;
+  }
+
+  function printUsersChoice() {
+    console.log("Users choice: " + usersChoice)
+    userChoiceEl.innerHTML = "Your choice: " + usersChoice;
+  }
+
+  function printResults(result) {
+    resultEl.innerHTML = "Result: " + result;
+  }
+
+  function reset() {
+    usersChoice = null;
+    userChoiceEl.innerHTML = "";
+    compChoiceEl.innerHTML = "";
+    resultEl.innerHTML = "";
+  }
+
 
   // listeners
   userNameField.addEventListener('keyup', function (event) {
@@ -63,17 +94,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   })
 
-  startButton.addEventListener('click', startGame);
+  startButton.addEventListener('click', renderGame);
+  submitButton.addEventListener('click', startGame);
 
   function startGame() {
-    game.className = "game-field-show";
-    registerChoice()
-
-    if (usersChoice != null) {
-      winLose( usersChoice,compChoice, rps)
+    if (tracker.length != 0) {
+      reset()
     }
-    else {
-      console.log("user choice was null")
-    }
+    makeChoice()
+    printCompsChoice()
+    winLose(cChoice, usersChoice, rps)
+    printTracker(tracker);
   }
 })
